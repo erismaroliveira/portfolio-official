93bf9468ef016ace1f1dcd59214f38648a626732
Erismar Oliveira <erismarpro@hotmail.com>
2021-08-06 15:27:45 -0300

    Add title iframe

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -13,7 +13,7 @@ function ContactPage() {
       </div>
       <div className="ContactPage">
       <div className="map-sect">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d245795.96135380736!2d-48.077446394422765!3d-15.721386971424437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a3d18df9ae275%3A0x738470e469754a24!2zQnJhc8OtbGlhLCBERg!5e0!3m2!1spt-BR!2sbr!4v1628105827636!5m2!1spt-BR!2sbr" width="600" height="450" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+        <iframe title="Meu endereço" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d245795.96135380736!2d-48.077446394422765!3d-15.721386971424437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a3d18df9ae275%3A0x738470e469754a24!2zQnJhc8OtbGlhLCBERg!5e0!3m2!1spt-BR!2sbr!4v1628105827636!5m2!1spt-BR!2sbr" width="600" height="450" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
       </div>
       <div className="contact-sect">
         <ContactItem 
